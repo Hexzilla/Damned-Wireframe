07fae89cbe4e62ab6d6b541e98f60c8da39df994
Eric Thomas <bluebird6922@gmail.com>
2022-06-12 04:18:20 +0300

    scroll

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -10,19 +10,18 @@ import { Settings } from "./types";
 
 export interface HomepageProps extends DefaultHomepageProps {}
 
-const defaultSettings = {
-  storyOpacity: 1,
-  leavesScale: 1,
-  overlayOpacity: 1,
-  lineTranslate: 0,
-  lineHeight: 0,
-} as Settings;
+// const defaultSettings = {
+//   storyOpacity: 1,
+//   leavesScale: 1,
+//   overlayOpacity: 1,
+//   lineTranslate: 0,
+//   lineHeight: 0,
+// } as Settings;
 
 function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
-  // const contentRef = useRef<HTMLDivElement>(null);
-  const leaveRef = useRef<HTMLImageElement>(null);
+  //const leaveRef = useRef<HTMLImageElement>(null);
   const [scroll, setScroll] = useState<number>(0);
-  const [settings, setSettings] = useState<Settings>(defaultSettings);
+  //const [settings, setSettings] = useState<Settings>(defaultSettings);
   const { height } = useWindowSize();
 
   const handleScrollEvent = () => {
@@ -31,46 +30,79 @@ function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
 
   useEffect(() => {
     window.addEventListener("scroll", handleScrollEvent);
+    const elements = document.getElementsByClassName("section");
+    for (let i = 0; i < elements.length; i++) {
+      const element = elements[i] as any;
+      element.style.zIndex = 100 - i;
+    }
     return () => {
       window.removeEventListener("scroll", handleScrollEvent);
     };
   }, []);
 
-  useEffect(() => {
-    const h = height / 1.5;
-    setSettings({
-      storyOpacity: scroll > 800 ? Math.max(1 - (scroll - h) / h, 0) : 1,
-      leavesScale: Math.min(scroll / h + 1, 1.75),
-      overlayOpacity: Math.min((scroll - h) / h, 1),
-      lineHeight: Math.max(Math.min((scroll - h) / h, 1), 0),
-      lineTranslate: Math.max(scroll - 1.5 * height, 0),
-    });
-  }, [scroll, height]);
+  // useEffect(() => {
+  //   window.addEventListener("scroll", handleScrollEvent);
+  //   return () => {
+  //     window.removeEventListener("scroll", handleScrollEvent);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   const h = height / 1.5;
+  //   setSettings({
+  //     storyOpacity: scroll > 800 ? Math.max(1 - (scroll - h) / h, 0) : 1,
+  //     leavesScale: Math.min(scroll / h + 1, 1.75),
+  //     overlayOpacity: Math.min((scroll - h) / h, 1),
+  //     lineHeight: Math.max(Math.min((scroll - h) / h, 1), 0),
+  //     lineTranslate: Math.max(scroll - 1.5 * height, 0),
+  //   });
+  // }, [scroll, height]);
+
+  // useEffect(() => {
+  //   if (leaveRef.current) {
+  //     const translate = 0; //-(100 * settings.leavesScale - 100);
+  //     leaveRef.current.style.transform = `scale(${settings.leavesScale}) translate3d(0, ${translate}px, 0)`;
+
+  //     if (settings.leavesScale === 1.75) {
+  //       leaveRef.current.style.opacity = "" + settings.storyOpacity;
+  //     }
+  //   }
+  //   // if (contentRef.current) {
+  //   //   contentRef.current.style.opacity = "" + settings.storyOpacity;
+  //   // }
+  // }, [settings]);
 
   useEffect(() => {
-    if (leaveRef.current) {
-      const translate = 0;//-(100 * settings.leavesScale - 100);
-      leaveRef.current.style.transform = `scale(${settings.leavesScale}) translate3d(0, ${translate}px, 0)`;
+    const h = height * 1.5;
 
-      if (settings.leavesScale === 1.75) {
-        leaveRef.current.style.opacity = "" + settings.storyOpacity;
-      }
+    const elements = document.getElementsByClassName("section");
+    const index = Math.floor(scroll / h);
+    if (index >= 0 && index < elements.length) {
+      const element = elements[index] as any;
+      const position = (scroll % h) / h;
+      
+      const scale = Math.min(1 + position, 1.75);
+      element.style.transform = `scale(${scale}) translate3d(0, 0, 0)`;
+
+      const opacity = (position >= 0.7) ? ((1 - position) / 0.3) : 1;
+      element.style.opacity = `${opacity}`;
     }
-    // if (contentRef.current) {
-    //   contentRef.current.style.opacity = "" + settings.storyOpacity;
-    // }
-  }, [settings]);
+  }, [scroll, height]);
 
   return (
     <div className="container">
       <div className="scene">
-        <div className="content">
-          <img
-            id="leaves-entrance"
-            src="/images/branding/habitat-static.webp"
-            alt="Leaves Entrance"
-            ref={leaveRef}
-          />
+        <div className="section">
+          <img src="/images/nft/Damned.jpg" alt="Leaves Entrance" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Lore.jpg" alt="Lore" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Roadmap.jpg" alt="Roadmap" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Limbo.jpg" alt="Limbo" />
         </div>
       </div>
     </div>
