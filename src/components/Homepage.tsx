310c6386307c725e5bd2f654da0c877ae5a53061
Eric Thomas <bluebird6922@gmail.com>
2022-06-12 04:34:55 +0300

    zoom effect

--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -6,22 +6,11 @@ import {
 import { HTMLElementRefOf } from "@plasmicapp/react-web";
 import "./Homepage.css";
 import useWindowSize from "../hooks/useWindowSize";
-import { Settings } from "./types";
 
 export interface HomepageProps extends DefaultHomepageProps {}
 
-// const defaultSettings = {
-//   storyOpacity: 1,
-//   leavesScale: 1,
-//   overlayOpacity: 1,
-//   lineTranslate: 0,
-//   lineHeight: 0,
-// } as Settings;
-
 function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
-  //const leaveRef = useRef<HTMLImageElement>(null);
   const [scroll, setScroll] = useState<number>(0);
-  //const [settings, setSettings] = useState<Settings>(defaultSettings);
   const { height } = useWindowSize();
 
   const handleScrollEvent = () => {
@@ -40,51 +29,31 @@ function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
     };
   }, []);
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleScrollEvent);
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScrollEvent);
-  //   };
-  // }, []);
-
-  // useEffect(() => {
-  //   const h = height / 1.5;
-  //   setSettings({
-  //     storyOpacity: scroll > 800 ? Math.max(1 - (scroll - h) / h, 0) : 1,
-  //     leavesScale: Math.min(scroll / h + 1, 1.75),
-  //     overlayOpacity: Math.min((scroll - h) / h, 1),
-  //     lineHeight: Math.max(Math.min((scroll - h) / h, 1), 0),
-  //     lineTranslate: Math.max(scroll - 1.5 * height, 0),
-  //   });
-  // }, [scroll, height]);
-
-  // useEffect(() => {
-  //   if (leaveRef.current) {
-  //     const translate = 0; //-(100 * settings.leavesScale - 100);
-  //     leaveRef.current.style.transform = `scale(${settings.leavesScale}) translate3d(0, ${translate}px, 0)`;
-
-  //     if (settings.leavesScale === 1.75) {
-  //       leaveRef.current.style.opacity = "" + settings.storyOpacity;
-  //     }
-  //   }
-  //   // if (contentRef.current) {
-  //   //   contentRef.current.style.opacity = "" + settings.storyOpacity;
-  //   // }
-  // }, [settings]);
-
   useEffect(() => {
-    const h = height * 1.5;
+    const h = height;
 
     const elements = document.getElementsByClassName("section");
     const index = Math.floor(scroll / h);
+    /*for (let i = 0; i < elements.length; i++) {
+      if (i !== index) {
+        const element = elements[i] as any;
+        element.style.opacity = `${0}`;
+      }
+    }*/
     if (index >= 0 && index < elements.length) {
-      const element = elements[index] as any;
       const position = (scroll % h) / h;
-      
-      const scale = Math.min(1 + position, 1.75);
+      if (position >= 0.2) {
+        if (index >= 1) {
+          const prev = elements[index - 1] as any;
+          prev.style.opacity = `${0}`;
+        }
+      }
+
+      const element = elements[index] as any;
+      const scale = (position > 0.1) ? Math.min(1 + position - 0.1, 1.75) : 1;
       element.style.transform = `scale(${scale}) translate3d(0, 0, 0)`;
 
-      const opacity = (position >= 0.7) ? ((1 - position) / 0.3) : 1;
+      const opacity = (position >= 0.5) ? ((1 - position) / 0.5) : 1;
       element.style.opacity = `${opacity}`;
     }
   }, [scroll, height]);
@@ -104,63 +73,32 @@ function Homepage_(props: HomepageProps, ref: HTMLElementRefOf<"div">) {
         <div className="section">
           <img src="/images/nft/Limbo.jpg" alt="Limbo" />
         </div>
+        <div className="section">
+          <img src="/images/nft/Lust.jpg" alt="Lust" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Gluttony.jpg" alt="Gluttony" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Greed.jpg" alt="Greed" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Anger.jpg" alt="Anger" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Heresy.jpg" alt="Heresy" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Violence.jpg" alt="Violence" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Fraud.jpg" alt="Fraud" />
+        </div>
+        <div className="section">
+          <img src="/images/nft/Team.jpg" alt="Team" />
+        </div>
       </div>
     </div>
-    // <div style={{ background: "black" }}>
-    //   <div className="dik-banner-2d">
-    //     <div className="banner-container">
-    //       <img
-    //         id="leaves-entrance"
-    //         src="/images/branding/leaves-entrance-cropped.png"
-    //         alt="Leaves Entrance"
-    //         ref={leaveRef}
-    //       />
-    //     </div>
-    //     <div className="banner-overlay" style={{ opacity: settings.overlayOpacity }}></div>
-    //     <div id="content" ref={contentRef}>
-    //       <PlasmicHomepage root={{ ref }} {...props} />
-    //     </div>
-    //     <div className="banner-container">
-    //       <img className="nft" src="/images/nft/Damned.jpg" alt="The Damned" />
-    //     </div>
-    //   </div>
-    //    <div className="rest-above">
-    //     <section id="Lore">
-    //       <img className="nft" src="/images/nft/Lore.jpg" alt="Lore" />
-    //     </section>
-    //     <section id="Roadmap">
-    //       <img className="nft" src="/images/nft/Roadmap.jpg" alt="Roadmap" />
-    //     </section>
-    //     <section id="Limbo">
-    //       <img className="nft" src="/images/nft/Limbo.jpg" alt="Limbo" />
-    //     </section>
-    //     <section id="Lust">
-    //       <img className="nft" src="/images/nft/Lust.jpg" alt="Lust" />
-    //     </section>
-    //     <section id="Gluttony">
-    //       <img className="nft" src="/images/nft/Gluttony.jpg" alt="Gluttony" />
-    //     </section>
-    //     <section id="Greed">
-    //       <img className="nft" src="/images/nft/Greed.jpg" alt="Greed" />
-    //     </section>
-    //     <section id="Anger">
-    //       <img className="nft" src="/images/nft/Anger.jpg" alt="Anger" />
-    //     </section>
-    //     <section id="Heresy">
-    //       <img className="nft" src="/images/nft/Heresy.jpg" alt="Heresy" />
-    //     </section>
-    //     <section id="Violence">
-    //       <img className="nft" src="/images/nft/Violence.jpg" alt="Violence" />
-    //     </section>
-    //     <section id="Fraud">
-    //       <img className="nft" src="/images/nft/Fraud.jpg" alt="Fraud" />
-    //     </section>
-    //     <section id="Team">
-    //       <img className="nft" src="/images/nft/Team.jpg" alt="Team" />
-    //     </section>
-    //   </div>
-    //   <div style={{ minHeight: "4082px" }} />
-    // </div>
   );
 }
 
